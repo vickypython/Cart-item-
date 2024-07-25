e91b489647af5667cda286d38d491789a5810058
vickypython <vickymlucky6@gmail.com>
2024-07-25 17:58:23 +0300

    updated cart component

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -2,41 +2,41 @@ import { FunctionComponent, useEffect, } from "react"
 import classes from './cart.module.scss'
 import Quantifier from '../Quantifier'
 import { useLocation } from "react-router-dom"
-import {TotalPrice} from '../TotalPrice'
+import { TotalPrice } from '../TotalPrice'
 import { CartProps } from "../Products/Products"
 import useLocalStorageState from 'use-local-storage-state'
 //operation
 export const Cart: FunctionComponent = () => {
     const [cart, setCart] = useLocalStorageState<CartProps>('cart', {})
-const location=useLocation()
-useEffect(()=>{
-    window.scrollTo(0,0)
-},[location])
+    const location = useLocation()
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location])
     const handleRemoveProduct = (productId: number): void => {
-        setCart((prevCart) => {
+        setCart((prevCart:CartProps) => {
             const updatedCart = { ...prevCart }
             delete updatedCart[productId]
             return updatedCart
         })
         const handleUpdateQuality = (productId: number, operation: string): void => {
-            setCart((prevCart) => {
+            setCart((prevCart:CartProps) => {
                 const updatedCart = { ...prevCart }
                 if (updatedCart[productId]) {
-                    if (operation ==='increase') {
+                    if (operation === 'increase') {
                         updatedCart[productId] = { ...updatedCart[productId], quantity: updatedCart[productId].quantity + 1 }
                     } else {
                         updatedCart[productId] = { ...updatedCart[productId], quantity: updatedCart[productId].quantity - 1 }
                     }
                 }
-return updatedCart
+                return updatedCart
             })
         }
 
 
     }
 
-const getProducts=()=>Object.values(cart||{})
-const totalPrice=getProducts().reduce((acc,product)=>acc+(product.price *product.quantity),0)
+    const getProducts = ():Product[] => Object.values(cart || {})
+    const totalPrice = getProducts().reduce((acc, product) => acc + (product.price * product.quantity), 0)
 
     return (
         <section className={classes.cart}>
@@ -47,8 +47,8 @@ const totalPrice=getProducts().reduce((acc,product)=>acc+(product.price *product
                         <img src={product.thumbnail} alt={product.title} />
                         <h3>{product.title}</h3>
                         <Quantifier
-                        removeProductCallback={()=>handleRemoveProduct(product.id)}
-                        handleUpdateQuality={ handleUpdateQuality}
+                            removeProductCallback={() => handleRemoveProduct(product.id)}
+                            handleUpdateQuality={handleUpdateQuality}
                             productId={product.id}
 
                         />
@@ -56,7 +56,7 @@ const totalPrice=getProducts().reduce((acc,product)=>acc+(product.price *product
                 ))}
 
             </div>
-            <TotalPrice amount={totalPrice}/>
+            <TotalPrice amount={totalPrice} />
         </section>
     )
 }
